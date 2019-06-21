02bb821141f8cf37d7c3a24f19742453b45931c9
davalcato <davalcato@yahoo.com>
2019-06-20 22:18:58 -0700

    first commit

---
+++ SampleApplication/SampleApplication/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  SampleApplication
+//
+//  Created by Daval Cato on 6/20/19.
+//  Copyright © 2019 Daval Cato. All rights reserved.
+//
+
+import UIKit
+import CameraFramework
+
+class ViewController: UIViewController {
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let camera = CameraViewController.init()
+        present(camera, animated: true, completion: nil)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

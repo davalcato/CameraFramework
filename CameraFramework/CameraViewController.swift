54aded5d5c7b4813d6aa0a7ae0efefe820b53a7c
davalcato <davalcato@yahoo.com>
2019-06-22 20:42:49 -0700

    added the public var position: CameraPosition = .back

--- CameraFramework/CameraViewController.swift
+++ CameraFramework/CameraViewController.swift
@@ -9,11 +9,15 @@
 import UIKit
 import AVFoundation
 
+public enum CameraPosition {
+    case front
+    case back
+}
+
 public final class CameraViewController: UIViewController {
+    public var position: CameraPosition = .back
     var session = AVCaptureSession()
-    var discoverySession: AVCaptureDevice.DiscoverySession?
-    {
-         // look through all devices 
+    var discoverySession: AVCaptureDevice.DiscoverySession? {
         return AVCaptureDevice.DiscoverySession(deviceTypes: [AVCaptureDevice.DeviceType.builtInWideAngleCamera], mediaType: AVMediaType.video, position: AVCaptureDevice.Position.unspecified)
     }
     var videoOutput = AVCaptureVideoDataOutput()
@@ -22,38 +26,34 @@ public final class CameraViewController: UIViewController {
         super.init(nibName: nil, bundle: nil)
         createUI()
         commitConfiguration()
-        
     }
     
-    required init?(coder: NSCoder) {
+    required public init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
     
     override public func viewDidLoad() {
         super.viewDidLoad()
         
+        // Do any additional setup after loading the view.
     }
     
     override public func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
     
     func createUI() {
         self.view.layer.addSublayer(getPreviewLayer(session: self.session))
-        
     }
     
     func commitConfiguration() {
-        
-        // set up input output
         do {
             guard let device = getDevice() else {
                 return
             }
             let input = try AVCaptureDeviceInput(device: device)
-            if self.session.canAddInput(input) && self.session.canAddOutput(self.videoOutput)
-            {
+            if self.session.canAddInput(input) && self.session.canAddOutput(self.videoOutput) {
                 self.session.addInput(input)
                 self.session.addOutput(self.videoOutput)
                 self.session.commitConfiguration()
@@ -63,31 +63,25 @@ public final class CameraViewController: UIViewController {
             print("Error linking device to AVInput!!")
             return
         }
-        
     }
     
-    func getPreviewLayer(session: AVCaptureSession) ->
-        AVCaptureVideoPreviewLayer {
-            let previewLayer = AVCaptureVideoPreviewLayer(session: session)
-            previewLayer.frame = self.view.bounds
-            return previewLayer
+    func getPreviewLayer(session: AVCaptureSession) -> AVCaptureVideoPreviewLayer {
+        let previewLayer = AVCaptureVideoPreviewLayer(session: self.session)
+        previewLayer.frame = self.view.bounds
+        return previewLayer
     }
     
     func getDevice() -> AVCaptureDevice? {
-        guard let discoverySession = self.discoverySession
-            else {
-                return nil
+        guard let discoverySession = self.discoverySession else {
+            return nil
         }
         
         for device in discoverySession.devices {
-            if device.position ==
-                AVCaptureDevice.Position.back {
+            if device.position == AVCaptureDevice.Position.back {
                 return device
             }
-            
         }
         return nil
-        
     }
     
 

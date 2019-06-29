2cd9f4c0c04d17c52c1028bc1302aaf067c1c221
davalcato <davalcato@yahoo.com>
2019-06-28 22:04:37 -0700

    added the  @IBAction func startButtonTapped so the user can started the camera manually.

--- SampleApplication/SampleApplication/ViewController.swift
+++ SampleApplication/SampleApplication/ViewController.swift
@@ -11,17 +11,23 @@ import CameraFramework
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        let camera = CameraViewController.init()
-        camera.delegate = self
-        camera.position = .back
-        present(camera, animated: true, completion: nil)
+        
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func startButtonTapped() {
+        let camera = CameraViewController.init()
+        camera.delegate = self
+        camera.position = .back
+        present(camera, animated: true, completion: nil)
     }
 }
 
